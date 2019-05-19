77a7132aa0ca1739986733dfbe1f8c84a7896e6c
Thomas Charlton <tchrlton1@gmail.com>
2019-05-19 02:37:48 -0700

    heroku push

--- src/config/main-config.js
+++ src/config/main-config.js
@@ -8,23 +8,25 @@ const viewsFolder = path.join(__dirname, "..", "views");
 const bodyParser = require("body-parser");
 
 module.exports = {
-  init(app, express){
-        app.set("views", viewsFolder);
-        app.set("view engine", "ejs");
-        app.use(bodyParser.urlencoded({ extended: true }));
-        app.use(express.static(path.join(__dirname, "..", "assets")));
-        app.use(expressValidator());
-        app.use(session({
-          secret: process.env.cookieSecret,
-          resave: false,
-          saveUninitialized: false,
-          cookie: { maxAge: 1.21e+9 }
-        }));
-        app.use(flash());
-        passportConfig.init(app);
-        app.use((req,res,next) => {
-          res.locals.currentUser = req.user;
-          next();
-        });  
+  init(app, express) {
+    app.set("views", viewsFolder);
+    app.set("view engine", "ejs");
+    app.use(bodyParser.urlencoded({ extended: true }));
+    app.use(express.static(path.join(__dirname, "..", "assets")));
+    app.use(expressValidator());
+    app.use(
+      session({
+        secret: process.env.cookieSecret,
+        resave: false,
+        saveUninitialized: false,
+        cookie: { maxAge: 1.21e9 }
+      })
+    );
+    app.use(flash());
+    passportConfig.init(app);
+    app.use((req, res, next) => {
+      res.locals.currentUser = req.user;
+      next();
+    });
   }
 };
